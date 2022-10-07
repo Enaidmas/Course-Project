556e50e29d3da0c9d1ff45763d4b48f039e9e90a
Enaidmas <111311138+Enaidmas@users.noreply.github.com>
2022-10-07 11:38:48 -0500

    Adding Dropdown Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ThingComponent } from './thing/thing.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FormsModule } from '@angular/forms';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingEditComponent,
-    ThingComponent
+    ThingComponent,
+    DropdownDirective
   ],
   imports: [
     FormsModule,

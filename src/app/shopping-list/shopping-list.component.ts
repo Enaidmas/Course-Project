a5ebd34f8384e69ddb005348e76ebbc78ca9ce31
Enaidmas <111311138+Enaidmas@users.noreply.github.com>
2022-10-12 16:34:51 -0500

    replacing event emitters with subjects

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,17 +8,23 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
 
   constructor(private shliService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shliService.getIngredients();
-    this.shliService.ingredientsChanged.subscribe(
+    this.igChangeSub = this.shliService.ingredientsChanged
+    .subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+  }
+
+  ngOnDestroy(): void {
+      this.igChangeSub.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,6 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 
@@ -7,7 +9,7 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>()
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 12),
@@ -18,7 +20,7 @@ export class ShoppingListService {
 
    addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
    }
 
    addIngredients(ingredients: Ingredient[]) {
@@ -26,7 +28,7 @@ export class ShoppingListService {
     //   this.addIngredient(ingredient);
     // ]
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
    }
   }
 
